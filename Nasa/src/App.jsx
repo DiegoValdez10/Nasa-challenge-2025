cf15e36f62d659c0702fc24df8d38cfc0aa6f2ad
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-10-05 18:32:30 -0600

    Refactor routing and styles in App component; update cupola styles for consistency

--- Nasa/src/App.jsx
+++ Nasa/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './view/home/home';
 import NBL from './view/nbl/nbl';
 import History from './view/history/history';
 import Cupola from './view/cupola/cupola';
 import Benefits from './view/benefits/benefits';
 import Navbar from './components/Navbar/navbar';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -24,7 +24,7 @@ function App() {
         </div>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
